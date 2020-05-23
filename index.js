1838cf4de6f47ed424c2018f7fe50ebe9a9d62c2
Tony MElguezabal <jose.elguezabal@gmail.com>
2020-05-23 11:16:00 -0500

    callback con request

--- index.js
+++ index.js
@@ -1,8 +1,37 @@
-const fs = require('fs')
+const request = require("request");
 
-function myCallback(data){
-  console.log(data)
-} 
+const options = {
+  url: "https://restcountries.eu/rest/v2/all",
+  method: "GET",
+  headers: {
+    Accept: "application/json",
+    "Accept-Charset": "utf-8",
+    "User-Agent": "my-reddit-client",
+  },
+};
 
-fs.readFile('./data.json')
+request(options, function (err, res, body) {
+  let json = JSON.parse(body);
+  console.log(json);
+});
 
+/*
+const fs = require("fs");
+
+function myCallback(data) {
+  console.log(data);
+}
+
+fs.readFile("./data.json");
+
+function traer() {
+  request 
+  fetch("https://restcountries.eu/rest/v2/all").then(function (response) {
+    response.json().then(function (data) {
+      objectopais = data;
+      console.log(objectopais);
+    });
+  });
+}
+
+*/
